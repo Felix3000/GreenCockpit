aa23e423393aeaf346855989470db64c59a46416
Felix3000 <Felix2000@felixgrsmacbook.fritz.box>
2014-05-16 18:16:50 +0200

    Revert "TestCommit"
    This reverts commit ecda9e3a1648652b6d04dfeb5bfb2b9df6260737.

--- htdocs/greencockpit/index.php
+++ htdocs/greencockpit/index.php
@@ -47,6 +47,8 @@
             <li><a href="?mode=controlling">Controlling</a></li>
             <li><a href="?mode=refresh">Refresh Data</a></li>
             <li><a href="?mode=help">Help</a></li>
+            <li><a href="settings.php">Settings</a></li>
+            <li><a href="?mode=impressum">Impressum</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
